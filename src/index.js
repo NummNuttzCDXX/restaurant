1cf6099c44f94835778fed8d69f174f46ca20b5f
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-19 16:33:40 -0400

    Import/Call createHeader and add to doc

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Main JS Script
+// Imports
+import { createHeader } from "./header";
+
+// Declare Vars
+const main = document.querySelector('div#content');
+
+// Create/Add header to doc
+main.appendChild(createHeader());
