0848d2c727acbe152916ed79025bcae6a15c2d7c
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-20 19:59:16 -0400

    Add url-loader as dependency to load images

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{  // This lets me add images using 'url-loader' package
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				use: {
+					loader: 'url-loader',
+				},
+			},
+		],
+	},
 };
