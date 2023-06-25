3584c633cbf5b19fae4cb2fc228dc0586f6b50c6
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-24 22:52:43 -0400

    Fix tab btns, Incorporate 'active' class on btns

--- src/index.js
+++ src/index.js
@@ -7,23 +7,26 @@ home();
 
 // Tab switching
 const tabBtns = document.querySelectorAll('header button');
-const content = Array.from(document.querySelector('#content').children);
 
 tabBtns.forEach(btn => {
 	btn.addEventListener('click', () => {
+		// Get all content elements
+		const content = Array.from(document.querySelector('#content').children);
 		// Remove all content children
-		content.forEach(element => {
-			element.remove()
-		})
+		content.forEach(element => element.remove())
 
 		const tab = btn.className;
+
+		// Add/remove 'active' class from btn
+		tabBtns.forEach(btn => btn.classList.remove('active'));
+		btn.classList.add('active');
 		
 		// Check tab and load New Content
-		if (tab === 'home') {
+		if (tab.includes('home')) {
 			body();
-		} else if (tab === 'menu') {
+		} else if (tab.includes('menu')) {
 			menu();
-		} else if (tab === 'contact') {
+		} else if (tab.includes('contact')) {
 			contact()
 		};
 	});
