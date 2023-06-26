1b862f63dc0efa1158d814bcd20e7f88c30315ad
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-26 19:49:07 -0400

    Create containers and headings for each contact element

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,24 +11,52 @@ export function createContact() {
 	container.appendChild(head);
 
 	// Address
+	const addCont = document.createElement('div');
+	const addHead = document.createElement('h3');
+	addHead.textContent = 'Address';
+	
 	const address = document.createElement('p');
 	address.textContent = '1234 NotAFake Rd YourCity, State';
-	container.appendChild(address);
+
+	addCont.appendChild(addHead);
+	addCont.appendChild(address);
+	container.appendChild(addCont);
 
 	// Hours
+	const hourCont = document.createElement('div');
+	const hourHead = document.createElement('h3');
+	hourHead.textContent = 'Hours';
+	
 	const hours = document.createElement('p');
-	hours.textContent = 'Everyday: 7am-11pm'
-	container.appendChild(hours);
+	hours.textContent = 'Everyday: 7am-11pm';
+
+	hourCont.appendChild(hourHead);
+	hourCont.appendChild(hours);
+	container.appendChild(hourCont);
 
 	// Phone
+	const phoneCont = document.createElement('div');
+	const phoneHead = document.createElement('h3');
+	phoneHead.textContent = 'Call us';
+
 	const phone = document.createElement('p');
 	phone.textContent = '(555)555-5555';
-	container.appendChild(phone);
+
+	phoneCont.appendChild(phoneHead);
+	phoneCont.appendChild(phone);
+	container.appendChild(phoneCont);
 
 	// Email
+	const mailCont = document.createElement('div');
+	const mailHead = document.createElement('h3');
+	mailHead.textContent = 'E-Mail';
+
 	const mail = document.createElement('p');
 	mail.textContent = 'definitelyRealEmail@notFake.com';
-	container.appendChild(mail);
+
+	mailCont.appendChild(mailHead);
+	mailCont.appendChild(mail);
+	container.appendChild(mailCont);
 
 	// Add Container
 	content.appendChild(container);
