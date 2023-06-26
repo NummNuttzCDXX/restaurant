1f7e80a8dac098e126efaa9c05b18dd64b52d5ee
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-06-26 15:51:47 -0400

    Add photo credits and id to item containers

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -24,10 +24,11 @@ export function createMenu() {
 	parentContainer.appendChild(menuHead);
 
 	// Create function to add/create items
-	function addItem(img, head, desc, price) {
+	function addItem(img, id, head, desc, price) {
 		// Create Item Container
 		const itemContainer = document.createElement('div');
 		itemContainer.classList.add('item');
+		itemContainer.setAttribute('id', id);
 
 		// Create/add image
 		const image = new Image();
@@ -55,6 +56,27 @@ export function createMenu() {
 		itemPrice.classList.add('price');
 		itemPrice.textContent = price;
 
+		// Add link
+		if (itemContainer.id === 'double') {
+			const link = document.createElement('a');
+			const txtBox = document.createElement('span');
+
+			link.href = 'https://www.freepik.com/free-ai-image/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_39518159.htm?log-in=google'
+			txtBox.textContent = 'Image By svstudioart';
+
+			link.appendChild(txtBox);
+			itemContainer.appendChild(link);
+		} else if (itemContainer.id === 'fry') {
+			const link = document.createElement('a');
+			const txtBox = document.createElement('span');
+
+			link.href = 'https://www.freepik.com/free-photo/side-view-french-fries-with-seasoning_8196330.htm#query=french%20fries&position=6&from_view=search&track=ais'
+			txtBox.textContent = 'Image by stockking'
+
+			link.appendChild(txtBox);
+			itemContainer.appendChild(link);
+		}
+
 		// Add containers to doc
 		itemContainer.appendChild(descContainer);
 		itemContainer.appendChild(itemPrice);
@@ -64,23 +86,23 @@ export function createMenu() {
 	// Menu
 	// Item 1
 	let doubleDesc = 'An all American double cheeseburger, topped with a sesame seed bun, lettuce, tomato, onions, and mayo.';
-	addItem(double, 'Double Cheeseburg', doubleDesc, '$8.99');
+	addItem(double, 'double', 'Double Cheeseburg', doubleDesc, '$8.99');
 
 	// Item 2
 	let bbqDesc = 'The perfect burger with bacon, sweet barbecue sauce, and onion rings. As if it couldn\'t get any better it\'s a double!';
-	addItem(bbq, 'BBQ Burg', bbqDesc, '$10.99');
+	addItem(bbq, 'bbq', 'BBQ Burg', bbqDesc, '$10.99');
 
 	// Item 3
 	let dogDesc = 'Have you ever had a hotdog and wished that it was a burger? Try our new Burger Dog! It\'s the best of both worlds!';
-	addItem(burgDog, 'Burger Dog', dogDesc, '$7.99');
+	addItem(burgDog, 'dog', 'Burger Dog', dogDesc, '$7.99');
 
 	// Item 4
 	let macBurgDesc = 'Original beef burg topped with bacon and mac n cheese ... YUM';
-	addItem(baconMac, 'Bacon Mac n Cheese Burg', macBurgDesc, '$10.99')
+	addItem(baconMac, 'bacon-mac', 'Bacon Mac n Cheese Burg', macBurgDesc, '$10.99')
 
 	// Item 5
 	let fryDesc = 'All time fan favorite seasoned french fries';
-	addItem(fries, 'Seasoned French Fries', fryDesc, '$2.99');
+	addItem(fries, 'fry', 'Seasoned French Fries', fryDesc, '$2.99');
 
 	// Add Menu-Container to parent
 	parentContainer.appendChild(menuContainer);
